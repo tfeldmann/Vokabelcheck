8d379a3eef563e600fce62458932b3a766006955
Thomas Feldmann <feldmann.thomas@googlemail.com>
2012-08-29 20:25:39 +0200

    Code cleanup

--- Vokabelcheck.py
+++ Vokabelcheck.py
@@ -1,16 +1,13 @@
 import string
 from Tkinter import *
 import latin_grammar as grammar
-import test_data
-
-fill_with_test_data = TRUE
 
 
 def get_words(text):
     for char in ["\n","\t","\r","!","-",".",",","?","'",'"',";","_","*","/",":"]:
         text = text.replace(char, " ")
     words = set(string.split(string.lower(text), " "))
-    print words
+
     return words
 
 
@@ -18,9 +15,8 @@ def missing_vocabulary(t, v):
     t = get_words(t) # words from the text
     v = get_words(v) # words from vocabs
 
-    # create giant set of vocabs
+    # remove known vocabulary
     missing = t - v
-
     for vocab in v:
         c = grammar.conjugate(vocab)
         d = grammar.declinate(vocab)
@@ -29,41 +25,13 @@ def missing_vocabulary(t, v):
 
     return missing
 
-    # # step 1: put all words that are not directly in the vocabs into the missing list
-    # print "Step 1:"
-    # for word in t:
-    #     if (word in v):
-    #         print "Found the word "+word+" in vocabulary."
-    #         continue
-    #     else:
-    #         missing_vocabulary.append(word)
-
-    # # step 2: check if the words can be build by conjugating or declirating the vocabs
-    # print "Step 2:"
-    # for missing in missing_vocabulary:
-    #     for vocab in v:
-
-    #         # check if they share a common base
-    #         if (missing.find(vocab, 0, len(vocab)) == 0):
-    #             print "The word "+missing+" could be a form of "+vocab
-
-    #             # check if it is a conjugation
-    #             if ((missing in grammar.conjugate(vocab)) or (missing in grammar.declinate(vocab))):
-    #                 missing_vocabulary.remove(missing)
-    #                 print "Confirmed."
-    #                 continue
-    #             else:
-    #                 print "But it is not."
-
-    # return missing_vocabulary
-
 
 def show_missing_vocabulary(text, vocabulary):
     mv = missing_vocabulary(text, vocabulary)
 
     rw = Tk()
     rw.focus_set()
-    result = Text(rw)
+    result = Text(rw, width = 25)
     resultScroll = Scrollbar(rw)
     result.configure(yscrollcommand = resultScroll.set)
     resultScroll.configure(command = result.yview)
@@ -105,11 +73,6 @@ def main():
     copyright = Label(controlFrame, text = '(c)2012 feldmann.thomas@googlemail.com', fg = 'gray')
     copyright.pack(side = LEFT, padx = 14, pady = 14)
     controlFrame.pack(fill = X)
-
-    if (fill_with_test_data):
-        latinText.insert(END, test_data.text)
-        vocabs.insert(END, test_data.vocabs)
-
 
     root.mainloop()
 
