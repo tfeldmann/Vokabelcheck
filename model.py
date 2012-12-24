9a375ee876035a3afa0c2c9d8c6f13261b1a479f
Thomas Feldmann <feldmann.thomas@googlemail.com>
2012-12-24 02:27:01 +0100

    added loading endings

--- model.py
+++ model.py
@@ -2,16 +2,14 @@
 import re
 
 def words_from_text(text):
-    text = re.sub('[^a-zA-Z]', ' ', text)
+    text = re.sub('[^a-zA-Z]', " ", text)
     text = text.rstrip('\r\n\t')
     words = text.split(" ")
     words = [word for word in words if word != ""]
     return words
 
-
 def word_with_endings(word, endings):
     return [word + ending for ending in endings]
-
 
 def words_with_endings(words, endings):
     _words = []
@@ -20,3 +18,20 @@ def words_with_endings(words, endings):
         for v in variations:
             _words.append(v)
     return _words
+
+def save_endings(endings, filename="endings.txt"):
+    f = open(filename, 'wb')
+    for ending in endings:
+        f.write(ending + "\n")
+    f.close()
+
+def load_endings(filename="endings.txt"):
+    with open(filename, 'rb') as f:
+        file_content = f.read()
+        lines = file_content.split("\n")
+
+        endings = []
+        for line in lines:
+            if line[:1] != "#" and line.strip() != "":
+                endings.append(line)
+        return endings
