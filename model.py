84fa76ba2a48204d7c20a7506642502ca6c22d37
Thomas Feldmann <feldmann.thomas@googlemail.com>
2012-12-27 14:50:34 +0100

    now works with sets instead of lists

--- model.py
+++ model.py
@@ -2,22 +2,34 @@
 import re
 
 def words_from_text(text):
+    """
+        returns an unordered set of words that are contained in the text.
+        Duplicates are removed
+    """
     text = re.sub('[^a-zA-Z]', " ", text)
     text = text.rstrip('\r\n\t')
     words = text.split(" ")
-    words = [word for word in words if word != ""]
+    words = {word for word in words if word != ""}
     return words
 
 def word_with_endings(word, endings):
-    return [word + ending for ending in endings]
+    return {word + ending for ending in endings}
 
 def words_with_endings(words, endings):
-    _words = []
+    result = set()
     for word in words:
-        variations = word_with_endings(word, endings)
-        for v in variations:
-            _words.append(v)
-    return _words
+        for variation in word_with_endings(word, endings):
+            result.add(variation)
+    return result
+
+def missing_vocabulary(words, basicforms, endings):
+    """
+        connect the basic forms with endings and check whether the words in the
+        "words" list are known. Returns a set of words that are missing.
+
+        words, basicforms and endings must be sets
+    """
+    result = set()
 
 def save_endings(endings, filename="endings.txt"):
     f = open(filename, 'wb')
--- tests.py
+++ tests.py
@@ -4,20 +4,33 @@ import model
 
 class VocabFindingTests(unittest.TestCase):
 
-    def test_words_from_text(self):
+    def test_words_from_text_parsing(self):
         words = model.words_from_text("!Abc &§$def,gh   iL..m\r\n\tx")
-        self.failUnlessEqual(words, ['Abc', 'def', 'gh', 'iL', 'm', 'x'])
+        self.failUnlessEqual(words, {'Abc', 'def', 'gh', 'iL', 'm', 'x'})
+
+    def test_words_from_text_duplicates(self):
+        words = model.words_from_text("A A A B C")
+        self.failUnlessEqual(words, {"A", "B", "C"})
 
     def test_word_with_endings(self):
         word = 'abc'
         x = model.word_with_endings(word, ['', 'b', 'aBc'])
-        self.failUnlessEqual(x, ['abc', 'abcb', 'abcaBc'])
+        self.failUnlessEqual(x, {'abc', 'abcb', 'abcaBc'})
+
+    def test_word_with_endings_duplicates(self):
+        word = "abc"
+        x = model.word_with_endings(word, {"x", "x", "y"})
+        self.failUnlessEqual(x, {"abcx", "abcy"})
 
     def test_words_with_endings(self):
         words = ['a', '', 'ABC']
-        x = model.words_with_endings(words, ['', 'x', 'XX'])
-        self.failUnlessEqual(x, ['a', 'ax', 'aXX', '', 'x', 'XX',
-            'ABC', 'ABCx', 'ABCXX'])
+        x = model.words_with_endings(words, {'', 'x', 'XX'})
+        self.failUnlessEqual(x, {'a', 'ax', 'aXX', '', 'x', 'XX',
+            'ABC', 'ABCx', 'ABCXX'})
+
+    def test_words_with_endings_duplicates(self):
+        x = model.words_with_endings(["a", "b", "b"], ["1", "2"])
+        self.failUnlessEqual(x, {"a1", "a2", "b1", "b2"})
 
 
 import os
